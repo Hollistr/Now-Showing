529d9cf12be0860ac62485ab5ea9cb8ee9f78922
hollistr <110345261+Hollistr@users.noreply.github.com>
2023-10-11 21:56:25 -0700

    added pages, components, and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Homepage from "./pages/Homepage";
+import MovieDetails from "./pages/MovieDetails";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <h1>Hello NowShowing</h1>
-      </div>
+      <Header />
+        <BrowserRouter>
+          <Routes>
+            <Route path={"/"} element={<Homepage />} />
+            <Route path={"/movieDetails"} element={<MovieDetails />} />
+            <Route path={"*"} element={<Homepage />} />
+          </Routes>
+        </BrowserRouter>
+
+
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
